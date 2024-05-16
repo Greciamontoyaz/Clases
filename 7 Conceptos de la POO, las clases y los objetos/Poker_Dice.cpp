02a4548c8a9806066d81ca565cc00056e0a0412d
Greciamontoyaz <67496742+Greciamontoyaz@users.noreply.github.com>
2024-05-16 12:07:11 -0600

    Update Poker_Dice.cpp

--- 7 Conceptos de la POO, las clases y los objetos/Poker_Dice.cpp
+++ 7 Conceptos de la POO, las clases y los objetos/Poker_Dice.cpp
@@ -7,121 +7,116 @@ using namespace std;
 
 const string labels[] = {"9", "10", "Jack", "Queen", "King", "Ace"};
 
-enum Hands {Five_of_a_kind,//
-            Four_of_a_kind,	//
-            Full_house,	//
-            Straight,	//
-            Three_of_a_kind,	//
-            Two_pair,	//
-            One_pair,	//
-            Bust };//
+enum Hands {
+    Five_of_a_kind,
+    Four_of_a_kind,
+    Full_house,
+    Straight,
+    Three_of_a_kind,
+    Two_pair,
+    One_pair,
+    Bust
+};
 
-
-class Player{
-  private:
+class Player {
+private:
     string name;
     int dice[5];
     Hands hand;
-    int *card;
     int mycount[6];
-    
-  public:
-    void roll_dice(){
-      for(int i=0; i<5; i++) {
-        dice[i]=rand()%6;       
-      }
-    }
-    void show_hand(){
-      for(int i=0; i<5; i++)
-        cout<<labels[ dice[i]] <<" \t";
-      cout<<endl;    
-    }
-  
-    void kind_of_hand(){
-      sort(dice, dice + 5);
-      for(int i=0; i<6; i++){
-       mycount[i]= count (dice, dice+5, i);
-      }
+    int highest_card;
 
-    if ( any_of(mycount, mycount+6, [](int i){return i==5;})){
-        cout << "There are five of a kind.\n";
-        hand = Five_of_a_kind;
-        card = find (mycount, mycount+6, 5);
-      }
-    else if ( any_of(mycount, mycount+6, [](int i){return i==4;})){
-         cout << "There are four of a kind.\n";
-         hand = Four_of_a_kind;
-       }
-    else if ( any_of(mycount, mycount+6, [](int i){return i==3;})           && any_of(mycount, mycount+6, [](int i){return i==2;})){
-         cout << "Full house.\n";
-         hand = Full_house;
-         card = find (mycount, mycount+6, 4);
-       }
-      /*In poker dice there are in fact only four possible bust         
-      hands: [A K Q J 9], [A K Q 10 9], [A K J 10 9], and [A Q         J 10 9];
-       both other no-pair hands (i.e., in which either         
-       the A or the 9 are missing) are straights.  */
+public:
+    void roll_dice() {
+        for (int i = 0; i < 5; i++) {
+            dice[i] = rand() % 6;
+        }
+    }
 
-    else if (all_of(mycount, mycount+6, [](int i){return i<=1;}) &&
-        (mycount[0]==0 || mycount[5]==0)){
-        cout << "Straight.\n";
-        hand = Straight;}        
-    else if(all_of(mycount, mycount+6, [](int i){return i<=1;}) && 
-        mycount[0]==1 && mycount[5]==1) {
-        cout << "Bust.\n";
-        hand = Bust;
-      }
-    else   if ( any_of(mycount, mycount+6, [](int i){return i==3;})           
-      && any_of(mycount, mycount+6, [](int i){return i==1;})){
-           cout << "Three of a kind.\n";
-           hand = Three_of_a_kind;
-           card = find (mycount, mycount+6, 3);
-         }
-    else if(count (mycount, mycount+6, 2)==2)
-          { cout << "Two pairs.\n";
-           hand = Two_pair;}
-    else if  ( any_of(mycount, mycount+6, [](int i){return i<=2;})           
-       && count (mycount, mycount+6, 1)==3){
-         cout << "One pair.\n";
-         hand = One_pair;
-         card = find (mycount, mycount+6, 2);
-       }
-       
+    void show_hand() {
+        for (int i = 0; i < 5; i++)
+            cout << labels[dice[i]] << "\t";
+        cout << endl;
+    }
+
+    void kind_of_hand() {
+        sort(dice, dice + 5);
+        fill(mycount, mycount + 6, 0);
+        
+        for (int i = 0; i < 5; i++) {
+            mycount[dice[i]]++;
+        }
+
+        if (any_of(mycount, mycount + 6, [](int i) { return i == 5; })) {
+            cout << "There are five of a kind.\n";
+            hand = Five_of_a_kind;
+        } else if (any_of(mycount, mycount + 6, [](int i) { return i == 4; })) {
+            cout << "There are four of a kind.\n";
+            hand = Four_of_a_kind;
+        } else if (any_of(mycount, mycount + 6, [](int i) { return i == 3; }) &&
+                   any_of(mycount, mycount + 6, [](int i) { return i == 2; })) {
+            cout << "Full house.\n";
+            hand = Full_house;
+        } else if (all_of(mycount, mycount + 6, [](int i) { return i <= 1; }) &&
+                   (mycount[0] == 0 || mycount[5] == 0)) {
+            cout << "Straight.\n";
+            hand = Straight;
+        } else if (all_of(mycount, mycount + 6, [](int i) { return i <= 1; }) &&
+                   mycount[0] == 1 && mycount[5] == 1) {
+            cout << "Bust.\n";
+            hand = Bust;
+        } else if (any_of(mycount, mycount + 6, [](int i) { return i == 3; })) {
+            cout << "Three of a kind.\n";
+            hand = Three_of_a_kind;
+        } else if (count(mycount, mycount + 6, 2) == 2) {
+            cout << "Two pairs.\n";
+            hand = Two_pair;
+        } else if (any_of(mycount, mycount + 6, [](int i) { return i == 2; })) {
+            cout << "One pair.\n";
+            hand = One_pair;
+        }
+        
+        highest_card = distance(mycount, max_element(mycount, mycount + 6));
     }
 
     friend class Dealer;
 };
 
-
-class Dealer{
-    public: 
-    void  CompareHand(Player player1,Player player2) {
-    if (player1.hand==player2.hand)
-        cout<<"The hands are equal.\n";
-     if(player1.hand<player2.hand)
-        cout<<"Player 1 wins.\n";
-     if(player1.hand>player2.hand)
-        cout<<"Player 2 wins.\n";
+class Dealer {
+public:
+    void CompareHand(Player player1, Player player2) {
+        if (player1.hand == player2.hand) {
+            cout << "The hands are equal.\n";
+            if (player1.highest_card == player2.highest_card) {
+                cout << "The highest cards are equal.\n";
+            } else if (player1.highest_card > player2.highest_card) {
+                cout << "Player 1 wins with a higher card.\n";
+            } else {
+                cout << "Player 2 wins with a higher card.\n";
+            }
+        } else if (player1.hand < player2.hand) {
+            cout << "Player 1 wins.\n";
+        } else {
+            cout << "Player 2 wins.\n";
+        }
     }
-    
-    
-     
 };
 
 int main() {
-srand (time(NULL));
+    srand(time(NULL));
 
-Player player1, player2;
-Dealer dealer;
-player1.roll_dice();
-player1.show_hand();
-player1.kind_of_hand();
+    Player player1, player2;
+    Dealer dealer;
 
+    player1.roll_dice();
+    player1.show_hand();
+    player1.kind_of_hand();
 
-player2.roll_dice();
-player2.show_hand();
-player2.kind_of_hand();
+    player2.roll_dice();
+    player2.show_hand();
+    player2.kind_of_hand();
 
-dealer.CompareHand(player1,player2);
+    dealer.CompareHand(player1, player2);
 
+    return 0;
 }
